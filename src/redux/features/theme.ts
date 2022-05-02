81f912762bc6ce699cefb1c5033c34fd49d84e56
francesco-camaione <francesco.camaione@gmail.com>
2022-05-02 15:03:29 +0200

    first commit

--- src/redux/features/theme.ts
+++ src/redux/features/theme.ts
@@ -1,9 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-// check user browser-theme and load website with that theme 
+// check for the user browser-theme and load website with that theme 
 const browser_theme: boolean = window.matchMedia("(prefers-color-scheme: dark)").matches
 const initialState: any = localStorage.getItem("theme") !== null ? localStorage.getItem("theme") : JSON.stringify({ theme: browser_theme ? "dark" : "light" })
-console.log(initialState)
 
 export const theme = createSlice({
     name: "theme",
